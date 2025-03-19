8e7c0311c0bed49d9d6f82592e78f482c708d33a
Timm Kühnel <t_kueh06@uni-muenster.de>
2025-03-19 17:08:51 +0100

    Start Implementing Chunk Struct

---
+++ src/chunk.rs
@@ -0,0 +1,55 @@
+use crc::Crc as Crc;
+
+use crate::chunk_type::ChunkType;
+
+#[derive(Clone, Debug)]
+pub struct Chunk {
+    chunk_type : ChunkType,
+    data : Vec<u8>,
+}
+
+impl Chunk {
+    pub fn length(&self) -> u32 {
+        self.data.len().try_into().unwrap()
+    }
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::chunk_type::ChunkType;
+    use std::str::FromStr;
+
+    fn testing_chunk() -> Chunk {
+        let data_length: u32 = 42;
+        let chunk_type = "RuSt".as_bytes();
+        let message_bytes = "This is where your secret message will be!".as_bytes();
+        let crc: u32 = 2882656334;
+
+        let chunk_data: Vec<u8> = data_length
+            .to_be_bytes()
+            .iter()
+            .chain(chunk_type.iter())
+            .chain(message_bytes.iter())
+            .copied()
+            .collect();
+
+        Chunk::try_from(chunk_data.as_ref()).unwrap()
+    }
+
+    #[test]
+    fn test_new_chunk() {
+        let chunk_type = ChunkType::from_str("RuSt").unwrap();
+        let data = "This is where your secret message will be!".as_bytes().to_vec();
+        let chunk = Chunk::new(chunk_type, data);
+        assert_eq!(chunk.length(), 42);
+        assert_eq!(chunk.crc(), 2882656334);
+    }
+
+    fn test_chunk_length() {
+        let chunk = testing_chunk();
+        assert_eq!(chunk.length(), 42);
+    }
+
+}
