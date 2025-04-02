4a0f0359b7e66320052da3e8e0a5f6cf2d2b0f1c
Timm Kühnel <t_kueh06@uni-muenster.de>
2025-04-02 13:20:17 +0200

    Refactor Struct Parameter Names and Commentary Style

--- src/args.rs
+++ src/args.rs
@@ -3,34 +3,32 @@ use std::path::PathBuf;
 
 use clap::Parser;
 
-use crate::chunk_type::ChunkType;
-
 #[derive(Debug, Parser)]
 #[command(author, version, about, long_about = None)]
 pub struct Commands {
     #[clap(subcommand)]
-    pub command : Args
+    pub command : PngMeArgs
 }
 
 #[derive(Debug, Parser, Clone)]
 #[command(name="pngme", about="Hide secret messages inside valid PNG files")]
-pub enum Args {
+pub enum PngMeArgs {
     Encode(EncodeArgs),
     Decode(DecodeArgs),
     Remove(RemoveArgs),
     Print(PrintArgs),
 }
 
-// Add a secret message into a PNG file
+/// add a secret message at the end of a PNG file, after EOF chunk
 #[derive(Debug, Parser, PartialEq, Clone)]
 #[command(version, about, long_about=None)]
 pub struct EncodeArgs {
     // file path for PNG file
     #[arg(short='f', long, value_name="PNG_PATH", value_hint=clap::ValueHint::DirPath)]
-    pub filepath: PathBuf,
+    pub input_path : PathBuf,
     // type of the new messages´ chunk
     #[arg(short='c', long, value_name="CHUNK_TYPE")]
-    pub chunk_type : ChunkType,
+    pub chunk_type : String,
     // message to encode in PNG file
     #[arg(short='m', long, value_name="MESSAGE")]
     pub message : String,
@@ -39,38 +37,38 @@ pub struct EncodeArgs {
     pub output_file : Option<PathBuf>,
 }
 
-// show the hidden message(s) in a PNG file
+/// show the hidden message(s) in a PNG file
 #[derive(Debug, Parser, PartialEq, Clone)]
 #[command(version, about, long_about=None)]
 pub struct DecodeArgs {
     // file path of PNG file
     #[arg(short='f', long, value_name="PNG_PATH", value_hint=clap::ValueHint::DirPath)]
-    pub filepath: PathBuf,
+    pub input_path: PathBuf,
     // type of the new messages´ chunk
     #[arg(short='c', long, value_name="CHUNK_TYPE")]
-    pub chunk_type : ChunkType,
+    pub chunk_type : String,
 }
 
-// 
+/// remove the first chunk matching matching the specified chunk type
 #[derive(Debug, Parser, PartialEq, Clone)]
 #[command(version, about, long_about=None)]
 pub struct RemoveArgs {
     // type of the (message) chunk to remove
     #[arg(short='c', long, value_name="CHUNK_TYPE")]
-    pub chunk_type : ChunkType,
+    pub chunk_type : String,
     // file path for PNG file
     #[arg(short='f', long, value_name="PNG_PATH", value_hint=clap::ValueHint::DirPath)]
-    pub filepath: PathBuf,
+    pub input_path: PathBuf,
     // optional: new file path for output PNG
     #[arg(short='o', long, value_name="NEW_FILEPATH", value_hint=clap::ValueHint::DirPath)]
     pub output_file : Option<PathBuf>,
 }
 
-
+/// print out the (raw) PNG file chunk by chunk 
 #[derive(Debug, Parser, PartialEq, Clone)]
 #[command(version, about, long_about=None)]
 pub struct PrintArgs {
     // file path for PNG file
     #[arg(short='f', long, value_name="PNG_PATH", value_hint=clap::ValueHint::DirPath)]
-    pub filepath: PathBuf,
+    pub input_path: PathBuf,
 }
